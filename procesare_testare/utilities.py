3ac2670e4718273f00852f2f80a5b662626b064b
adelp13 <adela.petre13@gmail.com>
2025-06-23 23:35:35 +0300

    .

--- procesare_testare/utilities.py
+++ procesare_testare/utilities.py
@@ -12,12 +12,13 @@ def dev_standard1(semnal):
     return semnal / std, std
 
 def plot_spectrograma(signal, i, sr=16000, n_fft=1024, hop_length=512, nume="spectrograma"):
+    # pt spectrogramele pe mix si cele 2 surse aproximate din mic
     plt.figure(figsize=(10, 4))
     S = librosa.stft(signal, n_fft=n_fft, hop_length=hop_length)
     S_db = librosa.amplitude_to_db(abs(S), ref=np.max)
     librosa.display.specshow(S_db, sr=sr, hop_length=hop_length, x_axis='time', y_axis='linear')
     plt.colorbar(format='%+2.0f dB')
-    plt.title(nume.upper(), fontsize=16, fontweight='bold')  # titlul mare și bold
+    plt.title(nume.upper(), fontsize=16, fontweight='bold')
     plt.tight_layout()
     plt.savefig(f"s_{nume}_{i}.pdf")
     plt.show()
